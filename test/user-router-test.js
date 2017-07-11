190145fd46f17a68f7d1866062e40570cb872721
Oscar Cauich <ocauich@gravitypayments.com>
2017-07-10 17:16:34 -0700

    test for user post route done, code 400

---
+++ test/user-router-test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+require('dotenv').config({path: `${__dirname}/../.test.env`});
+
+//npm modules
+const expect = require('expect');
+const superagent = require('superagent');
+
+//app modules
+const server = require('../lib/server.js');
+
+const API_URL = process.env.API_URL;
+
+describe('tesing user auth-router', () => {
+  before(server.start);
+  after(server.stop);
+
+  describe('tesing POST api/auth/register', () => {
+    it('should respond with a token', () => {
+      return superagent.post(`${API_URL}/api/auth/register`)
+        .send({
+          username: 'test_user',
+          password: 'top secret',
+          email: 'test@test.com',
+        })
+        .then(res => {
+          expect(res.status).toEqual(200);
+        });
+    });
+  });
+});
