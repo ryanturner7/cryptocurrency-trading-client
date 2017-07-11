89a2087b541a6bbf2957925c1830c4a75d878a1c
Devin G <engy100x@gmail.com>
2017-07-11 15:06:09 -0700

    working on put profile route

--- routes/profile-router.js
+++ routes/profile-router.js
@@ -2,6 +2,7 @@
 
 const {Router} = require('express');
 const jsonParser = require('body-parser');
+const bearerAuth = require('bearer-auth.js');
 
 const User = require('../model/user.js');
 const Trade = require('../model/trade.js');
@@ -13,3 +14,7 @@ profileRouter.get('/api/user', (req, res, next) => {
     .then(users => res.json(users.map(user => user._id)))
     .catch(next);
 });
+
+profileRouter.put('/api/user/profile', bearerAuth, jsonParser, (req, res, next) => {
+  
+})
