25797a88d038ba2b586833fb2a18eed4ad84b75f
Michael Stuart <mike.w.stuart@gmail.com>
2017-07-11 13:52:15 -0700

    set up trade post route

---
+++ model/trade.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const tradeSchema = mongoose.Schema({
+  serial: { type: String, required: true },
+  type: { type: String, required: true },
+  purchasedFrom: { type: mongoose.Schema.Types.ObjectId, required: true },
+  buyPrice: { type: Number, required: true },
+  buyDate: { type: Date, default: Date.now },
+  soldTo: { type: mongoose.Schema.Types.ObjectId },
+  sellPrice: { type: Number },
+  sellDate: { type: Date },
+  forSale: { type: Boolean, default: false },
+  askingPrice: { type: Number }
+});
+
+const Trade = module.exports = mongoose.model('trade', tradeSchema);
---
+++ routes/trade-router.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { Router } = require('express');
+const jsonParser = require('body-parser').json();
+const bearerAuth = require('../lib/bearer-auth');
+
+const User = require('../model/user');
+const Trade = require('../model/trade');
+
+const tradeRouter = module.exports = new Router();
+
+tradeRouter.post('/api/profile/trade', jsonParser, (req, res, next) => {
+  const coin = req.body.serial;
+  if(!coin || !buyPrice) return next(new Error('required arguments'));
+  Trade.find({ serial: coin }, (err, trade) => {
+    if(err) return next(new Error('bad request'));
+    const tradeCopy = Object.assign({}, trade);
+    const currentDate = new Date();
+    const price = req.body.buyPrice;
+    trade.soldTo = req.user._id;
+    trade.sellPrice = price;
+    trade.sellDate = currentDate;
+    trade.forSale = false;
+    trade.save(() => {
+    })
+  });
+});
