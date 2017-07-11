d408fda6cafe873f24f07e2085972f5f8b5944c5
Devin G <engy100x@gmail.com>
2017-07-11 16:06:28 -0700

    required mockUser

--- test/profile-router-test.js
+++ test/profile-router-test.js
@@ -4,6 +4,7 @@ const expect = require('expect');
 const superagent = require('superagent');
 const server = require('../lib/server.js');
 const cleanDB = require('./lib/clean-db.js');
+const mockUser = require('./lib/mock-user.js');
 
 const API_URL = process.env.API_URL;
 
