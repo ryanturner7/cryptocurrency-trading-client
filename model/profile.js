26adce42dbb023d25143d04ea510406e31bc0cd2
Oscar Cauich <ocauich@gravitypayments.com>
2017-07-11 23:56:44 -0700

    profile post test completed

--- model/profile.js
+++ model/profile.js
@@ -4,12 +4,11 @@ const mongoose = require('mongoose');
 
 const profileSchema = mongoose.Schema({
   userName:{type: String, required: true},
-  userId:{type: String},
+  userID:{type: mongoose.Schema.Types.ObjectId, required: true},
   trades:[{type: mongoose.Schema.Types.ObjectId, ref: '#'}],
   profilePic:{type: String},
   inbox:{type: String},
   outbox:{type: String},
 });
 
-const Profile = module.exports = mongoose.model('profile', profileSchema);
-// const User = module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model('profile', profileSchema);
