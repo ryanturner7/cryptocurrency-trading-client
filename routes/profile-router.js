e6c3864dae29808ed6b4d5d2a98087bd545f5243
Oscar Cauich <ocauich@gravitypayments.com>
2017-07-13 10:10:17 -0700

    put router for profiles working

--- routes/profile-router.js
+++ routes/profile-router.js
@@ -6,10 +6,10 @@ const s3Upload = require('../lib/s3-upload-handler.js');
 const bearerAuth = require('../lib/bearer-auth.js');
 const Profile = require('../model/profile.js');
 
+
 const profileRouter = module.exports = new Router();
 
 profileRouter.post('/api/profile/create', bearerAuth, s3Upload('profilePic'), (req, res, next) => {
-  console.log('s3Data', req.user);
   new Profile({
     userName: req.user.username,
     userID: req.user._id.toString(),
@@ -24,10 +24,21 @@ profileRouter.post('/api/profile/create', bearerAuth, s3Upload('profilePic'), (r
 });
 
 profileRouter.get('/api/profile/profile', bearerAuth, (req, res, next) => {
-  console.log('profile get', req.user);
+
   return Profile.find()
     .then(profile => {
       console.log('profile', profile);
+      res.json(profile);
+    })
+    .catch(next);
+});
+
+profileRouter.put('/api/profile/profile', bearerAuth, s3Upload('profilePic'), (req, res, next) => {
+  console.log('s3Data', req.body.userName);
+  let updateData = {userName: req.body.userName, profilePic: req.s3Data.Location};
+
+  return Profile.findOneAndUpdate({userID: req.user._id}, updateData)
+    .then(profile => {
       res.json(profile);
     })
     .catch(next);
