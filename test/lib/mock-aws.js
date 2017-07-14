496f20de6105478500aa3569a3510c9549132e9c
Michael Stuart <mike.w.stuart@gmail.com>
2017-07-14 01:05:00 -0700

    test libs

--- test/lib/mock-aws.js
+++ test/lib/mock-aws.js
@@ -3,19 +3,13 @@
 const awsMock = require('aws-sdk-mock');
 
 awsMock.mock('S3', 'upload', function(params, cb) {
-  console.log('hit awsmock');
-  if(params.ACL !== 'public-read')
-    return cb(new Error('ACL must be public-read'));
 
-  if(params.Bucket !== 'fake-bucket')
-    return cb(new Error('bucket must be fake-bucket'));
+  // console.log('hit awsmock');
 
-  if(!params.Key)
-    return cb(new Error('key must be set'));
-
-  if(!params.Body)
-    return cb(new Error('body must be set'));
-
+  if (params.ACL !== 'public-read') return cb(new Error('ACL must be public-read'));
+  if (params.Bucket !== 'fake-bucket') return cb(new Error('bucket must be fake-bucket'));
+  if (!params.Key) return cb(new Error('key must be set'));
+  if (!params.Body) return cb(new Error('body must be set'));
   cb(null, {
     Key: params.Key,
     Location: `fakeaws.s3.com/fake-bucket/${params.Key}`,
@@ -23,11 +17,7 @@ awsMock.mock('S3', 'upload', function(params, cb) {
 });
 
 awsMock.mock('S3', 'deleteObject', function(params, cb) {
-  if(!params.Key)
-    return cb(new Error('must have key set'));
-
-  if(!params.Bucket)
-    return cb(new Error('bucket must be fake-bucket'));
-
+  if (!params.Key) return cb(new Error('must have key set'));
+  if (!params.Bucket) return cb(new Error('bucket must be fake-bucket'));
   cb();
 });
--- test/lib/mock-user.js
+++ test/lib/mock-user.js
@@ -1,30 +1,26 @@
 'use strict';
 
 const faker = require('faker');
-const User = require('../../model/user.js');
+
+const User = require('../../model/user');
 
 const mockUser = module.exports = {};
 
-
 mockUser.createOne = () => {
-
   let result = {};
+
   result.password = faker.internet.password();
   return new User({
     username: faker.internet.userName(),
     email: faker.internet.email(),
-
   })
     .passwordHashCreate(result.password)
     .then(user => {
       result.user = user;
       return user.tokenCreate();
-
     })
-
     .then(token => {
       result.token = token;
       return result;
-
     });
 };
